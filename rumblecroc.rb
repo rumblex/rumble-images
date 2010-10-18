5c962f714188fb50859f327f85dda9658f973270
Jeff Rafter <jeff@baobabhealth.org>
2010-10-17 20:00:58 -0700

    Initial add

--- rumblecroc.rb
+++ rumblecroc.rb
@@ -1,6 +1,6 @@
 require 'rubygems'
 require 'cgi'
-require 'json'
+require 'ping'
 
 urls = []
 
@@ -10,10 +10,26 @@ File.open("./urls.txt", "r") do |infile|
   end
 end
 
+`mkdir -p full`
+`mkdir -p small`
+
 counter = 0
 
 urls.each do |url|
   puts CGI.escape(url) + " #{counter+=1}/#{urls.length}"
-  `crocodile #{url} images/#{CGI.escape(url)}.jpg`
+  # 30
+  # 58
+  # 151
+  # 261
+  # 273 ... popup alert
+  # 335
+next unless counter > 335
+  unless Ping.pingecho(url.gsub(/http\:\/\//, ''), 5, 80)
+    puts "============================================================================"
+    puts "Server not available for #{url}"
+    puts "============================================================================"
+    next
+  end
+  `crocodile #{url} full/#{CGI.escape(url)}.png`
+  `sips --resampleWidth 256 full/#{CGI.escape(url)}.png --out small/#{CGI.escape(url)}.png`
 end
-
